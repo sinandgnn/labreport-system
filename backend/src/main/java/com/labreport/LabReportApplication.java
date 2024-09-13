7227aa2e0e0ebc484f5587d8f20a29bbc3f452f8
Sinan <sinandgn00@gmail.com>
2024-09-13 12:05:00 +0300

    backend, added default technician for local use

--- backend/src/main/java/com/labreport/LabReportApplication.java
+++ backend/src/main/java/com/labreport/LabReportApplication.java
@@ -1,14 +1,40 @@
 package com.labreport;
 
+import com.labreport.entity.Technician;
+import com.labreport.repository.TechnicianRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class LabReportApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabReportApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner createTechnician(TechnicianRepository technicianRepository) {
+		return args -> {
+
+			String adminEmail = "admin@mail.com";
+			Technician inDB = technicianRepository.findByEmail(adminEmail);
+
+			if (inDB == null) {
+				PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+				Technician admin = new Technician();
+				admin.setEmail(adminEmail);
+				admin.setFullName("Default Technician");
+				admin.setPassword(passwordEncoder.encode("admin"));
+				admin.setHospitalIdentityNumber("1098375");
+				admin.setAdmin(true);
+				technicianRepository.save(admin);
+			}
+		};
 	}
 
 }
